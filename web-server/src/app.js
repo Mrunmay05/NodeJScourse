b0051bb1613235fbe2af31afd20216c64b7ead2f
Mrunmay05 <m9dixit@gmail.com>
2022-10-29 17:28:21 +0530

    fourth commit:web-server

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,26 +1,67 @@
+const path=require('path')
 const express=require('express')
-
+const hbs=require('hbs')
 const app=express()
 
-// app.com : whem somebody visits, we show them some respective page
-// app.com/help  //help route
-// app.com/about //about route
+const publicdirectorypath=path.join(__dirname,'../public')
+const viewpath=path.join(__dirname,'../templates/views')
+const partialpath=path.join(__dirname,'../templates/partials')
 
-app.get('',(req,res)=>{ //req stores info about incoming request to server
-//first parameter for get is the route, second is a function for when the route is visted
-    res.send('Hello Express!')  
+
+//set handlerbar engines and view location
+app.set('view engine','hbs')//set allows us to set a value for a given express setting
+app.set('views',viewpath)
+hbs.registerPartials(partialpath)// app.com : whem somebody visits, we show them some respective page// app.com/help  //help route// app.com/about //about route
+
+//setup static directory to server
+app.use(express.static(publicdirectorypath))//its a way to customize the server
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'weather-app',
+        name:'mrunmay'
+    })
+})//to input dynamic index.hbs
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        route_name:'about',
+        created_by:"mrunmay"
+    })
 })
 
 app.get('/help',(req,res)=>{
-    res.send('help page')
+    res.render('help',{
+        is_help:"provided",
+        provided_by:"mrunmay"
+    })
 })
 
-app.get('/about',(req,res)=>{
-    res.send('about page is on')
-})
+//we can also send json data through res.send(), automatically stringifies json
+// app.get('/help',(req,res)=>{
+//     res.send([{
+//         name:'mrunmay',
+//         age:20
+//     },{
+//         name:'dinya',
+//         age:19
+//     }])
+// })
+
+// app.get('/about',(req,res)=>{
+//     res.send('<h1>About page intensified</h1>')
+// })
 
 app.get('/weather',(req,res)=>{
-    res.send('weather is being displayed')
+    res.send([{
+        name_of_city: 'London',
+        Latitude:23.33,
+        Longitude:43.22
+    },{
+        name_of_city:'philadelphia',
+        Latitude:70.91,
+        Longitude:66.5
+    }])
 })
 
 app.listen(3000,()=>{
